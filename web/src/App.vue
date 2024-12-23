4d053787ab4fb46bc8781a2d3cf4961144c54468
haley huang <huanghongweimax@163.com>
2024-12-23 22:40:24 +0800

    add admin page

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,14 +2,16 @@
 import Home from './views/Home.vue'
 import router from './router';
 import Login from './views/login/Login.vue';
+import Admin from './views/admin/Admin.vue';
 
 
 </script>
 
 <template>
 
-  <router-view></router-view>
+  <!-- <router-view></router-view> -->
    <!-- <Login></Login> -->
+    <Admin></Admin>
 </template>
 
 <style scoped></style>
