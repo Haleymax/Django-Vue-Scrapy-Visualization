c7a2a5166ac48f8860f84cf913600abab8846833
haley huang <huanghongweimax@163.com>
2024-12-24 22:53:35 +0800

    add echart

---
+++ web/src/views/chart/SalesData.vue
@@ -0,0 +1,26 @@
+<template>
+    <div ref="" style="width: 600px; height: 400px;">
+
+    </div>
+</template>
+
+<script setup lang="ts" name="SalesChart">
+import { ref, onMounted, onUnmounted, nextTick } from 'vue';
+import * as echarts from 'echarts';
+
+const chartDom = ref(null);
+let chartInstance = null;
+
+onMounted(async () => {
+    await nextTick();
+    chartInstance = echarts.init(chartDom.value);
+    const option = {
+        title: {
+            text: 'ECharts 示例图'
+        },
+        tooltip: {},
+        
+    }
+})
+
+</script>
