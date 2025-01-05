e76462b289a5b1d8c7da006e630788ecde37545d
haley huang <huanghongweimax@163.com>
2025-01-05 23:08:14 +0800

    add sale data chart demo

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,15 +3,17 @@ import Home from './views/Home.vue'
 import router from './router';
 import Login from './views/login/Login.vue';
 import Admin from './views/admin/Admin.vue';
+import EchartsComponent from '@/views/chart/SalesData.vue'
 
 
 </script>
 
 <template>
 
-  <!-- <router-view></router-view> -->
+  <router-view></router-view>
    <!-- <Login></Login> -->
-    <Admin></Admin>
+    <!-- <Admin></Admin>
+     <EchartsComponent></EchartsComponent> -->
 </template>
 
 <style scoped></style>
